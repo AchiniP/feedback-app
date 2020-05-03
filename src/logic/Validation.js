53fbecdf9cba9250cccb57ef9ee5a3fc5865d565
achiniP <achi729@gmail.com>
2020-05-03 16:31:04 +0800

    feat(form): added screenshot'

--- src/logic/Validation.js
+++ src/logic/Validation.js
@@ -1,5 +1,5 @@
 const isValidEmail = (email) => {
   const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return EMAIL_REGEX.test(String(email).toLowerCase());
+  return email ? EMAIL_REGEX.test(String(email).toLowerCase()) : true;
 };
 export default isValidEmail;
